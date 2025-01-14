83baaa2ab5ed984a1821b59bd264f1d986a8a7bf
ENONGLOSKER <ENONGLOSKER@gmail.com>
2025-01-14 10:37:38 +0700

    add:al quran apps

---
+++ api_alquran_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiAlquranAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_alquran_app'
---
+++ api_alquran_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def alquran_list(request):
+    # surah_number = request.GET.get('surah', 1)
+    api_url = f"https://quran-api.santrikoding.com/api/surah"
+
+    response = requests.get(api_url)
+    data = response.json()
+
+    print('-------------------------')
+    print('DATA API :', data)
+
+    if response.status_code == 200:
+        return render(request, 'index.html', {'data': data})
+    else:
+        return render(request, 'index.html', {'data': None})
+def alquran_details(request, id):
+    if not id:
+        return render(request, 'error.html', {'error_message': 'ID is required'})
+
+    api_url = f"https://quran-api.santrikoding.com/api/surah/{id}"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Extract `ayat` and other relevant data
+        ayat = data.get('ayat', [])
+        surah_info = {
+            'nama_latin': data.get('nama_latin', ''),
+            'jumlah_ayat': data.get('jumlah_ayat', 0),
+            'tempat_turun': data.get('tempat_turun', ''),
+            'arti': data.get('arti', ''),
+            'audio': data.get('audio', ''),
+        }
+        return render(request, 'details.html', {'surah_info': surah_info, 'ayat': ayat})
+    else:
+        return render(request, 'error.html', {'error_message': 'Failed to retrieve data'})
